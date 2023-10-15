8c84a01dc1cafa84ddd2b2b15724d8eb6e7f8fa3
Justin Conner <122805784+Justin-Conner@users.noreply.github.com>
2023-10-15 16:54:02 -0500

    Add files via upload

---
+++ search.js
@@ -0,0 +1,5 @@
+function searchPages() {
+    var searchTerm = document.getElementById("searchInput").value;
+    // Perform search logic here, e.g., redirect to search results page or filter content on the current page.
+    alert("Searching for: " + searchTerm);
+}
